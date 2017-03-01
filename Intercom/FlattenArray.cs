18e8a52bbace961d624aebbafd75789cd802b456
KilskyreMan <ian.montgomery@outlook.ie>
2017-03-01 17:09:51 +0000

    Add project files.

---
+++ Intercom/FlattenArray.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Intercom
+{
+    public static class Interview
+    {
+        /// <summary>
+        /// Given an array of arbitrarily nested arrays of integers, convert into a flat array of integers. 
+        /// e.g. [[1,2,[3]],4] → [1,2,3,4].
+        /// 
+        /// Any exceptions raised will result in an empty array (see unit tests for examples).
+        /// </summary>
+        public static int[] FlattenArray(ArrayList toFlatten)
+        {
+            List<int> result = new List<int>();
+            try
+            {
+                FlattenRecursively(toFlatten, ref result);
+            }
+            catch
+            {
+                result.Clear();
+            }
+            return result.ToArray();
+        }
+
+        /// <summary>
+        /// Private method to flatten array. 
+        /// Will recurse through an element of the array if deemed to be an array itself.
+        /// If element is an integer - append to result, anything else - throw an exception.
+        /// </summary>
+        private static void FlattenRecursively(ArrayList toFlatten, ref List<int> result)
+        {
+            if (result == null)
+            {
+                result = new List<int>();
+            }
+
+            for (int i = 0; i < toFlatten.Count; ++i)
+            {
+                if (toFlatten[i] is ArrayList)
+                {
+                    FlattenRecursively(toFlatten[i] as ArrayList, ref result);
+                }
+                else if (toFlatten[i] is int)
+                {
+                    result.Add((int)toFlatten[i]);
+                }
+                else
+                {
+                    throw new Exception("Stop being silly, expected an arbitrary array of arrays or integers.");
+                }
+            }
+        }
+    }
+}
---
+++ IntercomTests/InterviewExerciseTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+
+namespace Intercom.Tests
+{
+    [TestClass()]
+    public class InterviewExerciseTests
+    {
+        [TestMethod()]
+        public void EmptyArray()
+        {
+            int[] emptyArray = new int[0];
+            CollectionAssert.AreEqual(emptyArray, Intercom.Interview.FlattenArray(null), "Null List");
+            CollectionAssert.AreEqual(emptyArray, Intercom.Interview.FlattenArray(new ArrayList()), "Empty List");
+        }
+
+        [TestMethod()]
+        public void SampleTest()
+        {
+            // [[1,2,[3]],4] → [1,2,3,4]
+            ArrayList sampleTest = new ArrayList() { new ArrayList() { 1, 2, new ArrayList() { 3 } } , 4 };
+
+            int[] expectedResult = new int[] { 1, 2, 3, 4 };
+            CollectionAssert.AreEqual(expectedResult, Intercom.Interview.FlattenArray(sampleTest));
+        }
+
+        [TestMethod()]
+        public void SimpleTest()
+        {
+            ArrayList simpleTest = new ArrayList() { 5, 4, 3, 2, 1 };
+
+            int[] expectedResult = new int[] { 5, 4, 3, 2, 1 };
+            CollectionAssert.AreEqual(expectedResult, Intercom.Interview.FlattenArray(simpleTest));
+        }
+
+        [TestMethod()]
+        public void SillyBuggerTest()
+        {
+            ArrayList sampleTest = new ArrayList()
+            {
+                new ArrayList() { 1, 2, new ArrayList() { "Three :)" } } , 4
+            };
+
+            int[] emptyArray = new int[0];
+            CollectionAssert.AreEqual(emptyArray, Intercom.Interview.FlattenArray(sampleTest));
+        }
+    }
+}
